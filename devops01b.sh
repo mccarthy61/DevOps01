55ff6b88c719c2ee96e890f2845ac2d9a031ed87
Mike McCarthy <mike_mccarthy@hotmail.com>
2020-04-18 10:30:12 -0700

    devops01b.sh

--- devops01b.sh
+++ devops01b.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
 # Initialize variables
+PART = 0
+CPU  = 0
+MEM  = 0
+IP   = 0
 
 # Test for parameters
 if [-z $1]
 then
     PART = 1
-    CPU = 1
-    MEM = 1
-    IP = 1
-elif [ 
+    CPU  = 1
+    MEM  = 1
+    IP   = 1
+elif [$1 == "-volumes"]
+    PART = 1
+elif [$1 == "-cpu"]
+    CPU  = 1
+elif [$1 == "-ram"]
+    RAM  = 1
+elif [$1 == "-network"]
+    IP   = 1
+elfi [$1 == "-all"]
+    PART = 1
+    CPU  = 1
+    MEM  = 1
+    IP   = 1
+fi 
 
 # Find out partition info
 df -Pk
