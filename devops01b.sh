a54e4abeced09a34924d0dacea49648da7690835
Mike McCarthy <mike_mccarthy@hotmail.com>
2020-04-18 10:52:43 -0700

    devops01b.sh

--- devops01b.sh
+++ devops01b.sh
@@ -1,53 +1,58 @@
 #!/bin/bash
 
 # Initialize variables
-PART = 0
-CPU  = 0
-MEM  = 0
-IP   = 0
+PART=0
+CPU=0
+MEM=0
+IP=0
 
 # Test for parameters
-if [-z $1]
+if [$1 == ""]
 then
-    PART = 1
-    CPU  = 1
-    MEM  = 1
-    IP   = 1
+    PART=0
+    CPU=0
+    MEM=0
+    IP=0
 elif [$1 == "-volumes"]
-    PART = 1
+then
+    PART=1
 elif [$1 == "-cpu"]
-    CPU  = 1
+then
+    CPU=1
 elif [$1 == "-ram"]
-    RAM  = 1
+then
+    RAM=1
 elif [$1 == "-network"]
-    IP   = 1
-elfi [$1 == "-all"]
-    PART = 1
-    CPU  = 1
-    MEM  = 1
-    IP   = 1
+then
+    IP=1
+elif [$1 == "-all"]
+then
+    PART=1
+    CPU=1 
+    MEM=1
+    IP=1
 fi 
 
 # Find out partition info
-if [PART]
+if [ $PART ]
 then
     df -Pk
 fi
 
 # Find Processor info
-if [CPU]
+if [ $CPU ]
 then
     lscpu
 fi
 
 # Find out Memory info
-if [MEM]
+if [ $MEM ]
 then
     cat /proc/meminfo
 fi
 
 # Find out network config
-if [IP]
+if [ $IP ]
 then
     ifconfig
 fi
